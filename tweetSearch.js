ba42800c62ba9e7148e5835d66f7bd2e25b9a0e4
Zivi Weinstock <zivi.weinstock@gmail.com>
2013-06-02 20:48:53 -0700

    Fixed formatting

--- tweetSearch.js
+++ tweetSearch.js
@@ -3,16 +3,16 @@ function tweets($scope, $http) {
 
   $scope.$watch("query", function () {
       $http.jsonp("http://search.twitter.com/search.json?callback=JSON_CALLBACK&q=" + $scope.query).then(function(results){
-      //will be called when get is finished
-      $scope.tweets = results.data.results;
+        //will be called when get is finished
+        $scope.tweets = results.data.results;
 
-      var readTweets = JSON.parse(localStorage.readTweets);
-      for (var i = 0; i < $scope.tweets.length; i++) {
-        if (readTweets.indexOf($scope.tweets[i].id) != -1) {
-          $scope.tweets[i].read = true;
+        var readTweets = JSON.parse(localStorage.readTweets);
+        for (var i = 0; i < $scope.tweets.length; i++) {
+          if (readTweets.indexOf($scope.tweets[i].id) != -1) {
+            $scope.tweets[i].read = true;
+          }
         }
-      }
-    })
+    });
   });
 
   $scope.query = "z1v1";
